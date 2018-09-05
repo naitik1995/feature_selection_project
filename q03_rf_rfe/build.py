433a5cf39e3471eea9970d3b09873cb791b30e2c
naitik1995 <naitikgada1995@gmail.com>
2018-09-05 17:55:56 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(data):
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    len_features = len(X.columns) / 2
+    random_est = RandomForestClassifier()
+    random_est.fit(X,y)
+    rfe_clf = RFE(random_est,n_features_to_select=len_features)
+    X_new= rfe_clf.fit_transform(X,y)
+    res = X.columns.values[rfe_clf.get_support()]
+    list_op = []
+    for i in res:
+        list_op.append(i)
+    return list_op    
+rf_rfe(data)
+
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -8,3 +9,17 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(data):
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    rf_clf = RandomForestClassifier()
+    rf_clf.fit(X,y)
+    model_select = SelectFromModel(rf_clf, prefit=True)
+    X_new = model_select.transform(X)
+    cols_data = X.columns.values[model_select.get_support()]
+    cols_list = []
+    for i in cols_data:
+        cols_list.append(i)
+    return cols_list    
+
+
